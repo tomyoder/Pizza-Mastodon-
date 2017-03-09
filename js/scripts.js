f231c49d58aaff5ff7c68884a0345be625472b97
Tom Yoder <motredoy@gmail.com>
2017-03-08 23:28:50 -0800

    improve html, js

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,37 @@
 //BACKEND
- function pizza(size, meat, veg, accent){
-   this.pizzaSize = size;
-   this.meatChoice = meat;
-   this.vegChoice = veg;
-   this.accentChoice = accent;
-   this.pizzaCost = 7;
+function Pizza(size, meat, veg, accent){
+  this.pizzaSize = size;
+  this.meatChoice = meat;
+  this.vegChoice = veg;
+  this.accentChoice = accent;
+  this.pizzaCost = 7;
  }
 
- pizza.prototype.price = function(size) {
-   if (this.pizzaSize === "large") {
-     return this.pizzaCost * 2
-        console.log(this.pizzaSize);
-   }
+
+
+Pizza.prototype.price = function(size) {
+  if (size === "large") {
+       console.log(size);
+    return this.pizzaCost * 2
+  }
     else if (size === "medium") {
-       return this.pizzaCost * 1.5;
-     }
-       else if (size === "small") {
-         return this.pizzaCost * 1;
-       }
+      return this.pizzaCost * 1.5;
+    }
+      else if (size === "small") {
+        return this.pizzaCost * 1;
+      }
 
   for (i=0; i<meatChoice.length; i++) {
          this.pizzaCost + 2;
-  })
+  }
   for (i=0; i<vegChoice.length; i++) {
          this.pizzaCost + 2;
-  })
+  }
   for (i=0; i<accentChoice.length; i++){
          this.pizzaCost + 2;
-  })
+  }
   return this.pizzaCost;
-
- };
+};
 
 //FRONTEND
  $(document).ready(function(){
@@ -40,9 +41,11 @@
      var size = $("input:radio[name=size]:checked").val();
      console.log(size);
 
-     $("input:checkbox[name=meat]:checked").each(function(){
-       var meat = $(this).val();
-       meatchoice.push(meat);
+     $("input:checkbox[name=meatChoice]:checked").each(function(){
+       var meatChoice = $(this).val();
+       var meat = [];
+       meat.push(meatChoice);
+       console.log(meat);
      });
 
      $("input:checkbox[name=vegetable]:checked").each(function(){
@@ -56,7 +59,8 @@
        accentchoice.push(accent)
 
      });
-     var pizzaChoice = new Pizza(size, meatChoice, vegChoice, accentChoice);
+
+     var pizzaChoice = new Pizza(size, meat, veg, accent);
      var pizzaPrice = user.pizza.price;
      //$("#price").text("Your Pizza will be " + priceResult + " dollars");
 
